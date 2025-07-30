ff64eea4790a33bfb0908e1c5bd3ccded81c0550
Janardhan Khandagle <khandaglejanardhan@gmail.com>
2025-07-30 13:02:21 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
+import About from './About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
+         <Route path="/about" element={<About/>} />
       </Routes>
     </BrowserRouter>
     
